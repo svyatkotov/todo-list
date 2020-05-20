57ef43f3cca772c39e6a6f372a8a0b4fbf9717c4
svyatkotov <skd946@gmail.com>
2020-05-20 21:02:35 +0300

    Updates

---
+++ precache-manifest.7e9e4a034326919eebbb345ea4b8d70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17db469de16b39e798d100f60b16a1f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "db4d982b2c34626240a4",
+    "url": "/todo-list/static/css/main.b4c45278.chunk.css"
+  },
+  {
+    "revision": "70606de5404ea5322a41",
+    "url": "/todo-list/static/js/2.5f527640.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.5f527640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4d982b2c34626240a4",
+    "url": "/todo-list/static/js/main.9aae4a37.chunk.js"
+  },
+  {
+    "revision": "8603314f6194160ae31b",
+    "url": "/todo-list/static/js/runtime-main.96edfc79.js"
+  }
+]);
